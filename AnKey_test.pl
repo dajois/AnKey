85fabdb5d5faa103ff22c17b2d7e7dcb2c175137
Noritomo Taba <e145709@ie.u-ryukyu.ac.jp>
2016-11-22 12:38:49 +0900

    2016/11/22

---
+++ AnKey_test.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+
+use strict;
+use YAML::Tiny;
+use Data::Dumper;
+
+my $yaml = YAML::Tiny->new;
+my $read = $yaml->read( 'data.yaml' );
+my $data = $read->[0];
+print Dumper($data);
